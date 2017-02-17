7786aa00885cfa9cd321fe820d386d173d3adb3d
Stan Lo <a22301613@yahoo.com.tw>
2017-02-17 19:02:07 +0800

    Make parser support 'self' token

--- parser/parser.go
+++ parser/parser.go
@@ -39,6 +39,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefix(token.IF, p.parseIfExpression)
+	p.registerPrefix(token.SELF, p.parseSelfExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
