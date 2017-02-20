de0e5621909ad2fe01e04339e94346000a536768
Stan Lo <a22301613@yahoo.com.tw>
2017-02-20 20:33:03 +0800

    Move primitive type definitions to their owen file.

--- object/object.go
+++ object/object.go
@@ -2,7 +2,6 @@ package object
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/st0012/rooby/ast"
 	"strings"
 )
@@ -28,52 +27,6 @@ type Object interface {
 	Inspect() string
 }
 
-type Integer struct {
-	Value int64
-}
-
-func (i *Integer) Type() ObjectType {
-	return INTEGER_OBJ
-}
-
-func (i *Integer) Inspect() string {
-	return fmt.Sprintf("%d", i.Value)
-}
-
-type String struct {
-	Value string
-}
-
-func (s *String) Type() ObjectType {
-	return STRING_OBJ
-}
-
-func (s *String) Inspect() string {
-	return s.Value
-}
-
-type Boolean struct {
-	Value bool
-}
-
-func (b *Boolean) Type() ObjectType {
-	return BOOLEAN_OBJ
-}
-
-func (b *Boolean) Inspect() string {
-	return fmt.Sprintf("%t", b.Value)
-}
-
-type Null struct{}
-
-func (n *Null) Type() ObjectType {
-	return NULL_OBJ
-}
-
-func (n *Null) Inspect() string {
-	return "null"
-}
-
 type ReturnValue struct {
 	Value Object
 }
@@ -84,18 +37,6 @@ func (r *ReturnValue) Type() ObjectType {
 
 func (r *ReturnValue) Inspect() string {
 	return r.Value.Inspect()
-}
-
-type Error struct {
-	Message string
-}
-
-func (e *Error) Type() ObjectType {
-	return ERROR_OBJ
-}
-
-func (e *Error) Inspect() string {
-	return "ERROR: " + e.Message
 }
 
 type Method struct {
---
+++ object/primitive_types.go
@@ -0,0 +1,61 @@
+package object
+
+import "fmt"
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType {
+	return STRING_OBJ
+}
+
+func (s *String) Inspect() string {
+	return s.Value
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+
+func (n *Null) Inspect() string {
+	return "null"
+}
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType {
+	return ERROR_OBJ
+}
+
+func (e *Error) Inspect() string {
+	return "ERROR: " + e.Message
+}
