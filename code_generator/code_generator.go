779f012c0380fc3010d7ae7bef319c8f8971d190
Stan Lo <a22301613@yahoo.com.tw>
2017-03-24 18:40:56 +0800

    Support compiling assign statement.
    Complete local variable store and retrieve.

--- code_generator/code_generator.go
+++ code_generator/code_generator.go
@@ -8,86 +8,116 @@ import (
 )
 
 type LocalTable struct {
-	store map[int]string
+	store map[string]int
+	count int
 }
 
-func (lt *LocalTable) Get(index int) (string, bool) {
-	obj, ok := lt.store[index]
-	return obj, ok
+func (lt *LocalTable) Get(v string) int {
+	return lt.store[v]
 }
 
-func (lt *LocalTable) Set(index int, val string) string {
-	lt.store[index] = val
-	return val
+func (lt *LocalTable) Set(val string) int {
+	c, ok := lt.store[val]
+
+	if !ok {
+		c = lt.count
+		lt.store[val] = c
+		lt.count += 1
+		return c
+	}
+
+	return c
 }
 
 type Scope struct {
-	Self    *ast.Statement
-	Program *ast.Program
-	Out     *Scope
+	Self       ast.Statement
+	Program    *ast.Program
+	Out        *Scope
+	LocalTable *LocalTable
 }
 
-var instructionSet []string
+type CodeGenerator struct {
+	program        *ast.Program
+	instructionSet []string
+}
 
-func GenerateByteCode(program *ast.Program) string {
-	scope := &Scope{Program: program}
-	compileProgram(program.Statements, scope)
-	instructions := strings.Join(instructionSet, "\n")
+func New(program *ast.Program) *CodeGenerator {
+	return &CodeGenerator{program: program}
+}
+
+func (cg *CodeGenerator) GenerateByteCode(program *ast.Program) string {
+	scope := &Scope{Program: program, LocalTable: newLocalTable()}
+	cg.compileProgram(program.Statements, scope)
+	instructions := strings.Join(cg.instructionSet, "\n")
 	return strings.TrimSpace(removeEmptyLine(instructions))
 }
 
-func compileProgram(stmts []ast.Statement, scope *Scope) {
+func (cg *CodeGenerator) compileProgram(stmts []ast.Statement, scope *Scope) {
 	var result []string
 
 	result = append(result, "<ProgramStart>")
 	for _, statement := range stmts {
-		s := &Scope{Self: &statement, Out: scope}
-		result = append(result, compileStatement(statement, s))
+		result = append(result, cg.compileStatement(statement, scope))
 	}
 
 	result = append(result, "leave")
-	instructionSet = append(instructionSet, strings.Join(result, "\n"))
+	cg.instructionSet = append(cg.instructionSet, strings.Join(result, "\n"))
 }
 
-func compileStatement(statement ast.Statement, scope *Scope) string {
+func (cg *CodeGenerator) compileStatement(statement ast.Statement, scope *Scope) string {
 	switch stmt := statement.(type) {
 	case *ast.ExpressionStatement:
-		return compileExpression(stmt.Expression, scope)
+		return cg.compileExpression(stmt.Expression, scope)
 	case *ast.DefStatement:
-		compileDefStmt(stmt, scope)
+		s := newScope(scope, stmt)
+		cg.compileDefStmt(stmt, s)
 		return ""
+	case *ast.AssignStatement:
+		return cg.compileAssignStmt(stmt, scope)
 	}
 
 	return ""
 }
 
-func compileDefStmt(stmt *ast.DefStatement, scope *Scope) {
+func (cg *CodeGenerator) compileAssignStmt(stmt *ast.AssignStatement, scope *Scope) string {
+	n := stmt.Name.ReturnValue()
+	index := scope.LocalTable.Set(n)
+	result := fmt.Sprintf(`
+%s
+setlocal %d
+`, cg.compileExpression(stmt.Value, scope), index)
+	return result
+}
+
+func (cg *CodeGenerator) compileDefStmt(stmt *ast.DefStatement, scope *Scope) {
 	var result []string
 
 	result = append(result, fmt.Sprintf("<Def:%s>", stmt.Name.Value))
 
 	for _, s := range stmt.BlockStatement.Statements {
-		result = append(result, compileStatement(s, scope))
+		result = append(result, cg.compileStatement(s, scope))
 	}
 	result = append(result, "leave")
-	instructionSet = append(instructionSet, strings.Join(result, "\n"))
+	cg.instructionSet = append(cg.instructionSet, strings.Join(result, "\n"))
 }
 
-func compileExpression(exp ast.Expression, scope *Scope) string {
+func (cg *CodeGenerator) compileExpression(exp ast.Expression, scope *Scope) string {
 	switch exp := exp.(type) {
+	case *ast.Identifier:
+		return fmt.Sprintf(`getlocal %d`, scope.LocalTable.Get(exp.Value))
 	case *ast.IntegerLiteral:
 		return fmt.Sprintf(`putobject %d`, exp.Value)
 	case *ast.InfixExpression:
-		return compileInfixExpression(exp, scope)
+		return cg.compileInfixExpression(exp, scope)
 	}
 
 	return ""
 }
 
-func compileInfixExpression(node *ast.InfixExpression, scope *Scope) string {
+func (cg *CodeGenerator) compileInfixExpression(node *ast.InfixExpression, scope *Scope) string {
 	var operation string
-	left := compileExpression(node.Left, scope)
-	right := compileExpression(node.Right, scope)
+	left := cg.compileExpression(node.Left, scope)
+	right := cg.compileExpression(node.Right, scope)
 	switch node.Operator {
 	case "+":
 		operation = "opt_plus"
@@ -115,4 +145,13 @@ func removeEmptyLine(s string) string {
 	s = regex.ReplaceAllString(s, "\n")
 
 	return s
+}
+
+func newLocalTable() *LocalTable {
+	s := make(map[string]int)
+	return &LocalTable{store: s}
+}
+
+func newScope(scope *Scope, stmt ast.Statement) *Scope {
+	return &Scope{Out: scope, LocalTable: newLocalTable(), Self: stmt}
 }
---
+++ code_generator/code_generator_test.go
@@ -0,0 +1,82 @@
+package code_generator
+
+import (
+	"github.com/st0012/Rooby/lexer"
+	"github.com/st0012/Rooby/parser"
+	"strings"
+	"testing"
+)
+
+func TestArithmeticCompilation(t *testing.T) {
+	input := `
+	(1 * 10 + 100) / 2
+	`
+
+	expected := `
+<ProgramStart>
+putobject 1
+putobject 10
+opt_mult
+putobject 100
+opt_plus
+putobject 2
+opt_div
+leave
+`
+
+	bytecodes := compileToBytecode(input)
+	compareBytecode(t, bytecodes, expected)
+}
+
+func TestLocalVariableAccessInCurrentScope(t *testing.T) {
+	input := `
+	a = 10
+	a = 100
+	b = 5
+	(b * a + 100) / 2
+	`
+	expected := `
+<ProgramStart>
+putobject 10
+setlocal 0
+putobject 100
+setlocal 0
+putobject 5
+setlocal 1
+getlocal 1
+getlocal 0
+opt_mult
+putobject 100
+opt_plus
+putobject 2
+opt_div
+leave
+`
+
+	bytecodes := compileToBytecode(input)
+	compareBytecode(t, bytecodes, expected)
+}
+
+func compileToBytecode(input string) string {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	p.CheckErrors()
+	cg := New(program)
+	return cg.GenerateByteCode(program)
+}
+
+func compareBytecode(t *testing.T, value, expected string) {
+	value = strings.TrimSpace(value)
+	expected = strings.TrimSpace(expected)
+	if value != expected {
+		t.Fatalf(`
+Bytecode compare failed
+Expect:
+%s
+
+Got:
+%s
+`, expected, value)
+	}
+}
