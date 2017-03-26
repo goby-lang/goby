c1a6b29dee04e1745a74f314230be6999ce91c7e
Stan Lo <a22301613@yahoo.com.tw>
2017-03-26 16:49:07 +0800

    Finish if expression's compilation.

--- code_generator/instruction.go
+++ code_generator/instruction.go
@@ -10,9 +10,13 @@ type Instruction struct {
 	Action string
 	Params []string
 	Line   int
+	Anchor *Anchor
 }
 
 func (i *Instruction) Compile() string {
+	if i.Anchor != nil {
+		return fmt.Sprintf("%d %s %d\n", i.Line, i.Action, i.Anchor.Line)
+	}
 	if len(i.Params) > 0 {
 		return fmt.Sprintf("%d %s %s\n", i.Line, i.Action, strings.Join(i.Params, " "))
 	}
@@ -22,6 +26,10 @@ func (i *Instruction) Compile() string {
 
 type Label struct {
 	Name string
+}
+
+type Anchor struct {
+	Line int
 }
 
 func (l *Label) Compile() string {
@@ -34,8 +42,22 @@ type InstructionSet struct {
 	Count        int
 }
 
-func (is *InstructionSet) Define(action string, params ...string) {
-	i := &Instruction{Action: action, Params: params, Line: is.Count}
+func (is *InstructionSet) Define(action string, params ...interface{}) {
+	ps := []string{}
+	i := &Instruction{Action: action, Params: ps, Line: is.Count}
+	for _, param := range params {
+		switch p := param.(type) {
+		case string:
+			ps = append(ps, p)
+		case *Anchor:
+			i.Anchor = p
+		}
+	}
+
+	if len(ps) > 0 {
+		i.Params = ps
+	}
+
 	is.Instructions = append(is.Instructions, i)
 	is.Count += 1
 }
