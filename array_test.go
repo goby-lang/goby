26648a21fce00309744fcaed637bc78c670faf1c
Stan Lo <a22301613@yahoo.com.tw>
2017-04-13 21:33:15 +0800

    Add integration test cases (copy from evaluator test).

---
+++ array_test.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"testing"
+	"github.com/st0012/Rooby/vm"
+)
+
+func TestEvalArrayExpression(t *testing.T) {
+	input := `
+	[1, "234", true]
+	`
+
+	evaluated := testEval(t, input)
+
+	arr, ok := evaluated.(*vm.ArrayObject)
+	if !ok {
+		t.Fatalf("Expect evaluated value to be an array. got=%T", evaluated)
+	}
+
+	testIntegerObject(t, arr.Elements[0], 1)
+	testStringObject(t, arr.Elements[1], "234")
+	testBooleanObject(t, arr.Elements[2], true)
+}
+
+func TestEvalArrayIndex(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`
+			[][1]
+		`, nil},
+		{`
+			[1, 2, 10, 5][2]
+		`, int64(10)},
+		{`
+			[1, "a", 10, 5][1]
+		`, "a"},
+		{`
+			a = [1, "a", 10, 5]
+			a[0]
+		`, 1},
+		{`
+			a = [1, "a", 10, 5]
+			a[2] = a[1]
+			a[2]
+
+		`, "a"},
+		{`
+			a = []
+			a[10] = 100
+			a[10]
+		`, 100},
+		{`
+			a = []
+			a[10] = 100
+			a[0]
+		`, nil},
+		{`
+			a = [1, 2 ,3 ,5 , 10]
+			a[0] = a[1] + a[2] + a[3] * a[4]
+			a[0]
+		`, 55},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(t, tt.input)
+
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, expected)
+		case string:
+			testStringObject(t, evaluated, expected)
+		case bool:
+			testBooleanObject(t, evaluated, expected)
+		case nil:
+			_, ok := evaluated.(*vm.Null)
+
+			if !ok {
+
+				t.Fatalf("expect input: \"%s\"'s result should be Null. got=%T(%s)", tt.input, evaluated, evaluated.Inspect())
+			}
+		}
+	}
+}
+
+func TestEachMethod(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{`
+		sum = 0
+		puts(self)
+		[1, 2, 3, 4, 5].each do |i|
+		  puts(self)
+		  puts(sum)
+		  sum = sum + i
+		end
+		sum
+		`, 15},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(t, tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
---
+++ hash_test.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"testing"
+	"github.com/st0012/Rooby/vm"
+)
+
+func TestEvalHashExpression(t *testing.T) {
+	input := `
+	{ foo: 123, bar: "test", baz: true }
+	`
+
+	evaluated := testEval(t, input)
+
+	h, ok := evaluated.(*vm.HashObject)
+	if !ok {
+		t.Fatalf("Expect evaluated value to be a hash. got=%T", evaluated)
+	}
+
+	for key, value := range h.Pairs {
+		switch key {
+		case "foo":
+			testIntegerObject(t, value, 123)
+		case "bar":
+			testStringObject(t, value, "test")
+		case "baz":
+			testBooleanObject(t, value, true)
+		}
+	}
+}
+
+func TestEvalHashAccess(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`
+			{}["foo"]
+		`, nil},
+		{`
+			{ bar: "foo" }["bar"]
+		`, "foo"},
+		{`
+			{ foo: 2, bar: "foo" }["foo"]
+		`, 2},
+		{`
+			h = { bar: "Foo" }
+			h["bar"]
+		`, "Foo"},
+		{`
+			h = { bar: 1, foo: 2 }
+			h["foo"] = h["bar"]
+			h["foo"]
+
+		`, 1},
+		{`
+			h = {}
+			h["foo"] = 100
+			h["foo"]
+		`, 100},
+		{`
+			h = {}
+			h["foo"] = 100
+			h["bar"]
+		`, nil},
+		{`
+			h = { foo: 1, bar: 5, baz: 10 }
+			h["foo"] = h["bar"] * h["baz"]
+			h["foo"]
+		`, 50},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(t, tt.input)
+
+		switch expected := tt.expected.(type) {
+		case int:
+			testIntegerObject(t, evaluated, expected)
+		case string:
+			testStringObject(t, evaluated, expected)
+		case bool:
+			testBooleanObject(t, evaluated, expected)
+		case nil:
+			_, ok := evaluated.(*vm.Null)
+
+			if !ok {
+
+				t.Fatalf("expect input: \"%s\"'s result should be Null. got=%T(%s)", tt.input, evaluated, evaluated.Inspect())
+			}
+		}
+	}
+}
