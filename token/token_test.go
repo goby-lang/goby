b0d49655782b2b0c2315f40d53fa6c1a5b3dd703
janczer <menshikov.ivn@gmail.com>
2017-04-22 16:44:18 +0200

    Add test to token.go

---
+++ token/token_test.go
@@ -0,0 +1,33 @@
+package token
+
+import "testing"
+
+func TestLookupIdentFalse(t *testing.T) {
+	token := LookupIdent("nonexist")
+	if token != IDENT {
+		t.Fatalf("Expect %s got %s", IDENT, token)
+	}
+}
+
+func TestLookupIdentTrue(t *testing.T) {
+	var keywords = map[string]TokenType{
+		"def":    DEF,
+		"true":   TRUE,
+		"false":  FALSE,
+		"if":     IF,
+		"else":   ELSE,
+		"return": RETURN,
+		"self":   SELF,
+		"end":    END,
+		"while":  WHILE,
+		"do":     DO,
+		"yield":  YIELD,
+	}
+
+	for name, token := range keywords {
+		test := LookupIdent(name)
+		if test != token {
+			t.Fatalf("Expect %s got %s", token, test)
+		}
+	}
+}
