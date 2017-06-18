bb9f460685631ba9e92733cc22c97611c8dda6f0
hachi8833 <hachi8833@gmail.com>
2017-06-18 18:50:42 +0900

    Add sleep before apache bench to avoid 'connection refused' error
    - Add 'rm -rf profile.out' for cleanup

--- test.sh
+++ test.sh
@@ -5,7 +5,6 @@ SLEEP=0.5
 set -e
 echo "" > coverage.txt
 
-rm -f profile.out
 for d in $(go list ./... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
@@ -19,7 +18,7 @@ done
 # TODO: Write a test for this specific case
 go install .
 goby test_fixtures/server.gb & PID=$!
-echo "Sleeping $SLEEP sec for waiting server.gb being ready..."; sleep $SLEEP
+echo "Sleeping for $SLEEP sec to wait server.gb being ready..."; sleep $SLEEP
 
 ab -n 3000 -c 100 http://localhost:3000/
 
