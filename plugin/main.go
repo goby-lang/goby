76d4491e09cc04f666c63d4832659f0a28ac3a13
Stan Lo <a22301613@yahoo.com.tw>
2017-07-20 17:58:01 +0800

    Update sample plugin and import.gb

--- plugin/main.go
+++ plugin/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type Bar struct {
+	name string
+}
+
+func (b *Bar) Name() {
+	fmt.Println(b.name)
+}
+
+func NewBar(name string) *Bar {
+	return &Bar{name: name}
+}
+
 func Foo() {
 	fmt.Println("Foo")
 }
