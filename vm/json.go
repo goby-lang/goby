3b5af55f70e434fba9efb518b0dbc79e42e831c5
Stan Lo <a22301613@yahoo.com.tw>
2017-08-15 20:59:41 +0800

    Implement json string to Goby object's transition.

---
+++ vm/json.go
@@ -0,0 +1,103 @@
+package vm
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+type jsonObj map[string]interface{}
+
+func initJSONClass(vm *VM) {
+	class := vm.initializeClass("JSON", false)
+	class.setBuiltInMethods(builtInJSONClassMethods(), true)
+	class.setBuiltInMethods(builtInJSONInstanceMethods(), false)
+	vm.objectClass.setClassConstant(class)
+}
+
+func builtInJSONClassMethods() []*BuiltInMethodObject {
+	return []*BuiltInMethodObject{
+		{
+			Name: "parse",
+			Fn: func(receiver Object) builtinMethodBody {
+				return func(t *thread, args []Object, blockFrame *callFrame) Object {
+					if len(args) != 1 {
+						return t.vm.initErrorObject(ArgumentError, "Expect 1 argument. got=%v", strconv.Itoa(len(args)))
+					}
+
+					j, ok := args[0].(*StringObject)
+
+					if !ok {
+						return t.vm.initErrorObject(TypeError, WrongArgumentTypeFormat, stringClass, args[0].Class().Name)
+					}
+
+					var obj jsonObj
+					var objs []jsonObj
+
+					jsonString := j.value
+
+					err := json.Unmarshal([]byte(jsonString), &obj)
+
+					if err != nil {
+						err = json.Unmarshal([]byte(jsonString), &objs)
+
+						if err != nil {
+							return t.vm.initErrorObject(InternalError, "Can't parse string %s as json: %s", jsonString, err.Error())
+						}
+
+						var objects []Object
+
+						for _, obj := range objs {
+							objects = append(objects, t.vm.convertJSONToHashObj(obj))
+						}
+
+						return t.vm.initArrayObject(objects)
+					}
+
+					return t.vm.convertJSONToHashObj(obj)
+				}
+			},
+		},
+	}
+}
+
+func builtInJSONInstanceMethods() []*BuiltInMethodObject {
+	return []*BuiltInMethodObject{}
+}
+
+func (v *VM) convertJSONToHashObj(j jsonObj) Object {
+	objectMap := map[string]Object{}
+
+	for key, jsonValue := range j {
+		switch jsonValue := jsonValue.(type) {
+		// Array of json objects
+		case []map[string]interface{}:
+			objs := []Object{}
+
+			for _, value := range jsonValue {
+				objs = append(objs, v.convertJSONToHashObj(value))
+			}
+
+			objectMap[key] = v.initArrayObject(objs)
+		case []interface{}:
+			objs := []Object{}
+
+			for _, elem := range jsonValue {
+				switch e := elem.(type) {
+				case map[string]interface{}:
+					objs = append(objs, v.convertJSONToHashObj(e))
+				default:
+					objs = append(objs, v.initObjectFromGoType(e))
+				}
+			}
+
+			objectMap[key] = v.initArrayObject(objs)
+		// Single json object
+		case map[string]interface{}:
+			objectMap[key] = v.convertJSONToHashObj(jsonValue)
+		default:
+			objectMap[key] = v.initObjectFromGoType(jsonValue)
+		}
+	}
+
+	return v.initHashObject(objectMap)
+}
---
+++ vm/json_test.go
@@ -0,0 +1,97 @@
+package vm
+
+import "testing"
+
+func TestJSONObjectParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`
+		require "json"
+		h = JSON.parse('{"Name": "Stan"}')
+		h["Name"]`, "Stan"},
+		{`
+		require "json"
+		h = JSON.parse('{"Age": 23}')
+		h["Age"]`, 23},
+		{`
+		require "json"
+		h = JSON.parse('
+		  {
+			"Project": {
+			  "Name": "Goby"
+			}
+		  }
+		')
+		h["Project"]["Name"]`, "Goby"},
+		{`
+		require "json"
+		h = JSON.parse('
+		  {
+			"Project": {
+			  "Name": "Goby",
+			  "Months": 7
+			}
+		  }
+		')
+		h["Project"]["Months"]`, 7},
+	}
+
+	for i, tt := range tests {
+		v := initTestVM()
+		evaluated := v.testEval(t, tt.input)
+		checkExpected(t, i, evaluated, tt.expected)
+		v.checkCFP(t, i, 0)
+		v.checkSP(t, i, 1)
+	}
+}
+
+func TestJSONObjectArrayParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`
+		require "json"
+		a = JSON.parse('[{"Name": "Stan"}]')
+		h = a.first
+		h["Name"]`, "Stan"},
+		{`
+		require "json"
+		a = JSON.parse('[{"Age": 23}]')
+		h = a.first
+		h["Age"]`, 23},
+		{`
+		require "json"
+		a = JSON.parse('
+		  [{
+			"Projects": [{
+			  "Name": "Goby"
+			}]
+		  }]
+		')
+		h = a.first
+		h["Projects"][0]["Name"]`, "Goby"},
+		{`
+		require "json"
+		a = JSON.parse('
+		  [{
+			"Projects": [{
+			  "Name": "Goby",
+			  "Months": 7
+			}]
+		  }]
+		')
+		h = a.first
+		h["Projects"][0]["Months"]`, 7},
+	}
+
+	for i, tt := range tests {
+		v := initTestVM()
+		evaluated := v.testEval(t, tt.input)
+		checkExpected(t, i, evaluated, tt.expected)
+		v.checkCFP(t, i, 0)
+		v.checkSP(t, i, 1)
+	}
+}
