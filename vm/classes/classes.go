e42027441605c8034cdab968c18c1021de0d5c25
st0012 <a22301613@yahoo.com.tw>
2017-09-06 21:06:17 +0800

    Add vm/classes package.

---
+++ vm/classes/classes.go
@@ -0,0 +1,18 @@
+package classes
+
+const (
+	ObjectClass   = "Object"
+	ClassClass    = "Class"
+	IntegerClass  = "Integer"
+	StringClass   = "String"
+	ArrayClass    = "Array"
+	HashClass     = "Hash"
+	BooleanClass  = "Boolean"
+	NullClass     = "Null"
+	ChannelClass  = "Channel"
+	RangeClass    = "Range"
+	MethodClass   = "method"
+	PluginClass   = "Plugin"
+	GoObjectClass = "GoObject"
+	FileClass     = "File"
+)
