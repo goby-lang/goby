42608361fc3b1c194043efbb92e3dd288fa9ea2d
Saverio Miroddi <saverio.pub2@gmail.com>
2017-10-04 21:33:09 +0200

    Improve Float test suite
    Improved existing test cases; added new ones; aligned test values.

--- vm/float_test.go
+++ vm/float_test.go
@@ -22,18 +22,39 @@ func TestFloatClassSuperclass(t *testing.T) {
 	}
 }
 
-func TestFloatArithmeticOperation(t *testing.T) {
+func TestFloatArithmeticOperationWithFloat(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected interface{}
 	}{
-		{`'1.5'.to_f + '2'.to_f`, 3.5},
-		{`'22.5'.to_f - '10'.to_f`, 12.5},
-		{`'5.5'.to_f * '20'.to_f`, 110.0},
-		{`'4.5'.to_f % '2'.to_f`, 0.5},
-		{`'10.5'.to_f % '3.5'.to_f`, 0.0},
-		{`'25.5'.to_f / '5'.to_f`, 5.1},
-		{`'5.5'.to_f ** '2'.to_f`, 30.25},
+    {`'13.5'.to_f  +  '3.2'.to_f`,  16.7},
+    {`'13.5'.to_f  -  '3.2'.to_f`,  10.3},
+    {`'13.5'.to_f  *  '3.2'.to_f`,  43.2},
+    {`'13.5'.to_f  %  '3.75'.to_f`, 2.25},
+    {`'13.5'.to_f  /  '3.75'.to_f`, 3.6},
+    {`'16.0'.to_f  ** '3.5'.to_f`,  16384.0},
+	}
+
+	for i, tt := range tests {
+		v := initTestVM()
+		evaluated := v.testEval(t, tt.input, getFilename())
+		checkExpected(t, i, evaluated, tt.expected)
+		v.checkCFP(t, i, 0)
+		v.checkSP(t, i, 1)
+	}
+}
+
+func TestFloatArithmeticOperationWithInteger(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+    {`'13.5'.to_f  +  3`, 16.5},
+    {`'13.5'.to_f  -  3`, 10.5},
+    {`'13.5'.to_f  *  3`, 40.5},
+    {`'13.5'.to_f  %  3`, 1.5},
+    {`'13.5'.to_f  /  3`, 4.5},
+    {`'13.5'.to_f  ** 3`, 2460.375},
 	}
 
 	for i, tt := range tests {
@@ -62,40 +83,57 @@ func TestFloatArithmeticOperationFail(t *testing.T) {
 	}
 }
 
-func TestFloatComparison(t *testing.T) {
+func TestFloatComparisonWithFloat(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected interface{}
 	}{
-		{`'25.5'.to_f > '5'.to_f`, true},
-		{`'4.5'.to_f > '6'.to_f`, false},
-		{`'-5.5'.to_f < '-4'.to_f`, true},
-		{`'100.5'.to_f < '81'.to_f`, false},
-		{`'25.5'.to_f > '5'.to_f`, true},
-		{`'4.5'.to_f > '6'.to_f`, false},
-		{`'4.5'.to_f >= '4'.to_f`, true},
-		{`'2.5'.to_f >= '5'.to_f`, false},
-		{`'-5.5'.to_f < '-4'.to_f`, true},
-		{`'100.5'.to_f < '81'.to_f`, false},
-		{`'10.5'.to_f <= '10.5'.to_f`, true},
-		{`'10.5'.to_f <= '0'.to_f`, false},
-		{`'10.5'.to_f <=> '0'.to_f`, 1},
-		{`'1.5'.to_f <=> '2'.to_f`, -1},
-		{`'4.5'.to_f <=> '4.5'.to_f`, 0},
-		{`'123.5'.to_f == '123.5'.to_f`, true},
-		{`'123.5'.to_f == '124'.to_f`, false},
-		{`'123.5'.to_f == "'123'.to_f"`, false},
-		{`'123.5'.to_f == (1..3)`, false},
-		{`'123.5'.to_f == { a: '1'.to_f, b: '2'.to_f }`, false},
-		{`'123.5'.to_f == ['1'.to_f, "String", true, 2..5]`, false},
-		{`'123.5'.to_f == Float`, false},
-		{`'123.5'.to_f != '123.5'.to_f`, false},
-		{`'123.5'.to_f != '124'.to_f`, true},
-		{`'123.5'.to_f != '123'.to_f`, true},
-		{`'123.5'.to_f != (1..3)`, true},
-		{`'123.5'.to_f != { a: '1'.to_f, b: '2'.to_f }`, true},
-		{`'123.5'.to_f != ['1'.to_f, "String", true, 2..5]`, true},
-		{`'123.5'.to_f != Float`, true},
+    {`'1.5'.to_f >   '2.5'.to_f`,  false},
+    {`'2.5'.to_f >   '1.5'.to_f`,  true},
+    {`'3.5'.to_f >   '3.5'.to_f`,  false},
+    {`'1.5'.to_f <   '2.5'.to_f`,  true},
+    {`'2.5'.to_f <   '1.5'.to_f`,  false},
+    {`'3.5'.to_f <   '3.5'.to_f`,  false},
+    {`'1.5'.to_f >=  '2.5'.to_f`,  false},
+    {`'2.5'.to_f >=  '1.5'.to_f`,  true},
+    {`'3.5'.to_f >=  '3.5'.to_f`,  true},
+    {`'1.5'.to_f <=  '2.5'.to_f`,  true},
+    {`'2.5'.to_f <=  '1.5'.to_f`,  false},
+    {`'3.5'.to_f <=  '3.5'.to_f`,  true},
+    {`'1.5'.to_f <=> '2.5'.to_f`,  -1},
+    {`'2.5'.to_f <=> '1.5'.to_f`,  1},
+    {`'3.5'.to_f <=> '3.5'.to_f`,  0},
+	}
+
+	for i, tt := range tests {
+		v := initTestVM()
+		evaluated := v.testEval(t, tt.input, getFilename())
+		checkExpected(t, i, evaluated, tt.expected)
+		v.checkCFP(t, i, 0)
+		v.checkSP(t, i, 1)
+	}
+}
+
+func TestFloatComparisonWithInteger(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+    {`'1'.to_f >   2`,  false},
+    {`'2'.to_f >   1`,  true},
+    {`'3'.to_f >   3`,  false},
+    {`'1'.to_f <   2`,  true},
+    {`'2'.to_f <   1`,  false},
+    {`'3'.to_f <   3`,  false},
+    {`'1'.to_f >=  2`,  false},
+    {`'2'.to_f >=  1`,  true},
+    {`'3'.to_f >=  3`,  true},
+    {`'1'.to_f <=  2`,  true},
+    {`'2'.to_f <=  1`,  false},
+    {`'3'.to_f <=  3`,  true},
+    {`'1'.to_f <=> 2`,  -1},
+    {`'2'.to_f <=> 1`,  1},
+    {`'3'.to_f <=> 3`,  0},
 	}
 
 	for i, tt := range tests {
@@ -121,6 +159,38 @@ func TestFloatComparisonFail(t *testing.T) {
 		evaluated := v.testEval(t, tt.input, getFilename())
 		checkError(t, i, evaluated, tt.expected, getFilename(), tt.errorLine)
 		v.checkCFP(t, i, 1)
+		v.checkSP(t, i, 1)
+	}
+}
+
+func TestFloatEquality(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`'123.5'.to_f  ==  '123.5'.to_f`, true},
+		{`'123'.to_f    ==  123`,          true},
+		{`'123.5'.to_f  ==  '124'.to_f`,   false},
+		{`'123.5'.to_f  ==  "123.5"`,      false},
+		{`'123.5'.to_f  ==  (1..3)`,       false},
+		{`'123.5'.to_f  ==  { a: 1 }`,     false},
+		{`'123.5'.to_f  ==  [1]`,          false},
+		{`'123.5'.to_f  ==  Float`,        false},
+		{`'123.5'.to_f  !=  '123.5'.to_f`, false},
+		{`'123.5'.to_f  !=  123`,          true},
+		{`'123.5'.to_f  !=  '124'.to_f`,   true},
+		{`'123.5'.to_f  !=  "123.5"`,      true},
+		{`'123.5'.to_f  !=  (1..3)`,       true},
+		{`'123.5'.to_f  !=  { a: 1 }`,     true},
+		{`'123.5'.to_f  !=  [1]`,          true},
+		{`'123.5'.to_f  !=  Float`,        true},
+	}
+
+	for i, tt := range tests {
+		v := initTestVM()
+		evaluated := v.testEval(t, tt.input, getFilename())
+		checkExpected(t, i, evaluated, tt.expected)
+		v.checkCFP(t, i, 0)
 		v.checkSP(t, i, 1)
 	}
 }
