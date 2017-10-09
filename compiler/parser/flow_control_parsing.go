ea30c5a2902d943af90ff934f0384baa9f927742
st0012 <a22301613@yahoo.com.tw>
2017-10-09 17:26:21 +0800

    Extract flow control parsing.

---
+++ compiler/parser/flow_control_parsing.go
@@ -0,0 +1,124 @@
+package parser
+
+import (
+	"github.com/goby-lang/goby/compiler/ast"
+	"github.com/goby-lang/goby/compiler/token"
+)
+
+// Case expression forms if statement when parsing it
+//
+// ```ruby
+// case 1
+// when 0, 1
+//  '0 or 1'
+// else
+//  'else'
+// end
+// ```
+//
+// is the same with if expression below
+//
+// ```ruby
+// if 1 == 0 || 1 == 1
+//  '0 or 1'
+// else
+//  'else'
+// end
+// ```
+//
+// TODO Implement '===' method and replace '==' to '===' in Case expression
+
+func (p *Parser) parseCaseExpression() ast.Expression {
+	ie := &ast.IfExpression{BaseNode: &ast.BaseNode{Token: p.curToken}}
+	ie.Conditionals = p.parseCaseConditionals()
+
+	if p.curTokenIs(token.Else) {
+		ie.Alternative = p.parseBlockStatement()
+		ie.Alternative.KeepLastValue()
+	}
+
+	return ie
+}
+
+// case expression parsing helpers
+func (p *Parser) parseCaseConditionals() []*ast.ConditionalExpression {
+	p.nextToken()
+	base := p.parseExpression(NORMAL)
+
+	p.expectPeek(token.When)
+	ce := []*ast.ConditionalExpression{}
+
+	for p.curTokenIs(token.When) {
+		ce = append(ce, p.parseCaseConditional(base))
+	}
+
+	return ce
+}
+
+func (p *Parser) parseCaseConditional(base ast.Expression) *ast.ConditionalExpression {
+	ce := &ast.ConditionalExpression{BaseNode: &ast.BaseNode{Token: p.curToken}}
+	p.nextToken()
+
+	ce.Condition = p.parseCaseCondition(base)
+	ce.Consequence = p.parseBlockStatement()
+	ce.Consequence.KeepLastValue()
+
+	return ce
+}
+
+func (p *Parser) parseCaseCondition(base ast.Expression) *ast.InfixExpression {
+	first := p.parseExpression(NORMAL)
+	infix := p.formInfixExpression(base, token.Eq, first)
+
+	for p.peekTokenIs(token.Comma) {
+		p.nextToken()
+		p.nextToken()
+
+		right := p.parseExpression(NORMAL)
+		rightInfix := p.formInfixExpression(base, token.Eq, right)
+		infix = p.formInfixExpression(infix, token.Or, rightInfix)
+	}
+
+	if p.peekTokenIs(token.Then) {
+		p.nextToken()
+	}
+
+	return infix
+}
+
+func (p *Parser) parseIfExpression() ast.Expression {
+	ie := &ast.IfExpression{BaseNode: &ast.BaseNode{Token: p.curToken}}
+	// parse if and elsif expressions
+	ie.Conditionals = p.parseConditionalExpressions()
+
+	// curToken is now ELSE or RBRACE
+	if p.curTokenIs(token.Else) {
+		ie.Alternative = p.parseBlockStatement()
+		ie.Alternative.KeepLastValue()
+	}
+
+	return ie
+}
+
+// infix expression parsing helpers
+func (p *Parser) parseConditionalExpressions() []*ast.ConditionalExpression {
+	// first conditional expression should start with if
+	cs := []*ast.ConditionalExpression{p.parseConditionalExpression()}
+
+	for p.curTokenIs(token.ElsIf) {
+		cs = append(cs, p.parseConditionalExpression())
+	}
+
+	return cs
+}
+
+func (p *Parser) parseConditionalExpression() *ast.ConditionalExpression {
+	ce := &ast.ConditionalExpression{BaseNode: &ast.BaseNode{Token: p.curToken}}
+	p.nextToken()
+	ce.Condition = p.parseExpression(NORMAL)
+
+	ce.Consequence = p.parseBlockStatement()
+	ce.Consequence.KeepLastValue()
+
+	return ce
+}
