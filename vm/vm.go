d202c4b2660db2f9644064a04ee6013a1774e295
st0012 <a22301613@yahoo.com.tw>
2017-12-02 22:59:11 +0800

    Bump to v0.1.6

--- vm/vm.go
+++ vm/vm.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Version stores current Goby version
-const Version = "0.1.3"
+const Version = "0.1.6"
 
 // These are the enums for marking parser's mode, which decides whether it should pop unused values.
 const (
