fdba9aa1298d07ff08cb849d33d3997264385a9a
st0012 <a22301613@yahoo.com.tw>
2018-02-12 09:43:07 +0800

    Apply new pattern to call expression tests.

--- compiler/ast/test_helper_types.go
+++ compiler/ast/test_helper_types.go
@@ -2,8 +2,6 @@
 
 package ast
 
-import "testing"
-
 const nodeFailureMsgFormat = "Node is not %s, is %T"
 
 type TestingIdentifier string
@@ -11,21 +9,4 @@ type TestingIdentifier string
 type testingNode interface {
 	// Belows are test helpers
 	NameIs(name string) bool
-}
-
-type TestingExpression interface {
-	Expression
-	// Test Helpers
-	IsArrayExpression(t *testing.T) *ArrayExpression
-	IsAssignExpression(t *testing.T) *AssignExpression
-	IsCallExpression(t *testing.T) *CallExpression
-	IsConditionalExpression(t *testing.T) *ConditionalExpression
-	IsConstant(t *testing.T) *Constant
-	IsHashExpression(t *testing.T) *HashExpression
-	IsIdentifier(t *testing.T) *Identifier
-	IsIfExpression(t *testing.T) *IfExpression
-	IsInfixExpression(t *testing.T) *InfixExpression
-	IsIntegerLiteral(t *testing.T) *IntegerLiteral
-	IsStringLiteral(t *testing.T) *StringLiteral
-	IsYieldExpression(t *testing.T) *YieldExpression
 }
---
+++ compiler/ast/testable_expressions.go
@@ -0,0 +1,44 @@
+package ast
+
+import "testing"
+
+type TestingExpression interface {
+	Expression
+	// Test Helpers
+	IsArrayExpression(t *testing.T) *ArrayExpression
+	IsAssignExpression(t *testing.T) *AssignExpression
+	IsCallExpression(t *testing.T) *TestableCallExpression
+	IsConditionalExpression(t *testing.T) *ConditionalExpression
+	IsConstant(t *testing.T) *Constant
+	IsHashExpression(t *testing.T) *HashExpression
+	IsIdentifier(t *testing.T) *Identifier
+	IsIfExpression(t *testing.T) *IfExpression
+	IsInfixExpression(t *testing.T) *InfixExpression
+	IsIntegerLiteral(t *testing.T) *IntegerLiteral
+	IsStringLiteral(t *testing.T) *StringLiteral
+	IsYieldExpression(t *testing.T) *YieldExpression
+}
+
+/*TestableCallExpression*/
+
+type TestableCallExpression struct {
+	*CallExpression
+	t *testing.T
+}
+
+// NthArgument returns n-th argument of the call expression as TestingExpression
+func (tce *TestableCallExpression) NthArgument(n int) TestingExpression {
+	return tce.Arguments[n-1].(TestingExpression)
+}
+
+// ReceiverExpression returns call expression's receiver as TestingExpression
+func (tce *TestableCallExpression) ReceiverExpression() TestingExpression {
+	return tce.Receiver.(TestingExpression)
+}
+
+// ShouldHasMethodName
+func (tce *TestableCallExpression) ShouldHasMethodName(expectedName string) {
+	if tce.Method != expectedName {
+		tce.t.Fatalf("expect call expression's method name to be '%s', got '%s'", expectedName, tce.Method)
+	}
+}
