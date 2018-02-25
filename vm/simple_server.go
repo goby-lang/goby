ad299641f8a28304028c51d462465a762ad5f6f9
st0012 <stan001212@gmail.com>
2018-02-25 10:23:26 +0800

    Fix SimpleServer's port assigning issue.

--- vm/simple_server.go
+++ vm/simple_server.go
@@ -13,6 +13,8 @@ import (
 	"github.com/fatih/structs"
 	"github.com/goby-lang/goby/vm/classes"
 	"github.com/gorilla/mux"
+	"fmt"
+	"strconv"
 )
 
 type request struct {
@@ -71,12 +73,19 @@ func builtinSimpleServerInstanceMethods() []*BuiltinMethodObject {
 					var serveStatic bool
 					server := receiver.(*RObject)
 
-					portVar, ok := server.InstanceVariables.get("@port")
+					portVar, ok := server.instanceVariableGet("@port")
 
 					if !ok {
 						port = "8080"
 					} else {
-						port = portVar.(*StringObject).value
+						switch p := portVar.(type) {
+						case *StringObject:
+							port = p.value
+						case *IntegerObject:
+							port = strconv.Itoa(p.value)
+						default:
+							fmt.Printf("Unexpected type %s for port setting\n", portVar.Class().Name)
+						}
 					}
 
 					log.Println("SimpleServer start listening on port: " + port)
