a1d2f7f4a803df442f4084d66875c737581af854
hachi8833 <hachi8833@gmail.com>
2018-05-23 19:57:15 +0900

    Update vm/errors/error.go for message integration

--- vm/errors/error.go
+++ vm/errors/error.go
@@ -21,14 +21,45 @@ const (
 	HTTPError = "HTTPError"
 	// ZeroDivisionError is for zero-division by Integer/Float/Decimal value
 	ZeroDivisionError = "ZeroDivisionError"
+	// BlockError is for indicating errors regarding block
+	BlockError = "BlockError"
+	// FileError is for indicating errors regarding files
+	FileError = "FileError"
 )
 
-/*
-	Here defines different error message formats for different types of errors
-*/
+// Error messages for native methods
 const (
-	WrongNumberOfArgumentFormat = "Expect %d arguments. got: %d"
-	WrongArgumentTypeFormat     = "Expect argument to be %s. got: %s"
+	WrongNumberOfArgumentFormat      = "Expects %d argument(s). got: %d"
+	WrongNumberOfArgumentFormatMore  = "Expects %d or more argument(s). got: %d"
+	WrongNumberOfArgumentFormatRange = "Expects %d to %d argument(s). got: %d"
+	WrongArgumentTypeFormat          = "Expects argument to be %s. got: %s"
+	WrongArgumentSignNegative        = "Expects argument to be positive. got: %d"
+
+	IndexValueTooSmall = "Index value %d is too small for array. minimum: %d"
+	UndefinedMethodFor = "Undefined method '%s' for %s"
+
 	CantYieldWithoutBlockFormat = "Can't yield without a block"
-	DividedByZero               = "Divided by 0"
+	CantCallClass               = "Can't call class on %T"
+	CantCompleteRequest         = "Can't complete request: %s"
+
+	CantInitializeBlockObjectWithoutBlockArgument = "Can't initialize block object without a block argument"
+
+	InvalidFileMode = "Invalid file mode: %"
+
+	DividedByZero = "Divided by 0"
+
+	CantParseStringAsJson = "Can't parse string %s as json: %s"
+
+	CantCreatePlubing = "Can't create plugin: %s"
+
+	ShouldBePositiveInteger = "Step should be a positive integer."
+)
+
+// Error messages for VM internal errors
+const (
+	UninitializedConstant            = "Uninitialized constant: %s"
+	CantGetInstructionSet            = "Can't get instruction set from method: %s"
+	ConstantIsNotClass               = "Constant %s is not a class. got: %s"
+	ModuleInheritanceUnsupported     = "Module inheritance is unsupported: %s"
+	CantGetBlockWithoutBlockArgument = "Can't get block without a block argument"
 )
