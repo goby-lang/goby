cd12032295a318b90fa32f6759b9ef547748e254
eliothedeman <eliot.d.hedeman@gmail.com>
2018-06-18 22:45:44 -0400

    Render basic binding functions

--- cmd/binder/main.go
+++ cmd/binder/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
@@ -16,14 +18,54 @@ var (
 	out = flag.String("out", "bindings", "output file name")
 )
 
+const (
+	vmPkg = "github.com/goby-lang/goby/vm"
+)
+
 type Binding struct {
 	ClassName       string
 	ClassMethods    []*ast.FuncDecl
 	InstanceMethods []*ast.FuncDecl
 }
 
-func (Binding) New() {
+func (b *Binding) staticName() string {
+	return fmt.Sprintf("_static%s", b.ClassName)
+}
 
+func (b *Binding) bindingName(f *ast.FuncDecl) string {
+	return fmt.Sprintf("_binding_%s_%s", b.ClassName, f.Name.Name)
+}
+
+func (b *Binding) BindMethods(f *jen.File) {
+	for _, c := range b.ClassMethods {
+		b.BindClassMethod(f, c)
+		f.Line()
+	}
+	for _, c := range b.InstanceMethods {
+		b.BindInstanceMethod(f, c)
+		f.Line()
+	}
+}
+
+func (b *Binding) BindClassMethod(f *jen.File, d *ast.FuncDecl) {
+	f.Func().Id(b.bindingName(d)).Call().Block()
+}
+
+func (b *Binding) BindInstanceMethod(f *jen.File, d *ast.FuncDecl) {
+	s := f.Func().Id(b.bindingName(d))
+	s = s.Params(jen.Id("r").Qual(vmPkg, "Object"), jen.Id("line").Id("int")).Qual(vmPkg, "Method")
+	ff := jen.Func().Params(jen.Id("t").Qual(vmPkg, "Thread"), jen.Id("args").Index().Qual(vmPkg, "Object"))
+	ff = ff.Qual(vmPkg, "Object")
+	ff = ff.Block(
+		jen.Return(nil),
+	)
+	s.Block(jen.Return(ff))
+
+	// func closeDB(receiver vm.Object, sourceLine int) vm.Method {
+	// 	return func(t *vm.Thread, args []vm.Object) vm.Object {
+	// 	}
+	// 	return nil
+	// }
 }
 
 func main() {
@@ -40,7 +82,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var binding Binding
+	bindings := make(map[string]*Binding)
 
 	ast.Inspect(f, func(n ast.Node) bool {
 		switch n := n.(type) {
@@ -48,30 +90,45 @@ func main() {
 			if n.Recv != nil {
 				// class or instance?
 				r := n.Recv.List[0]
-				r.Type.
+				var name string
+				switch t := r.Type.(type) {
+				case *ast.Ident:
+					name = t.Name
+
+				case *ast.StarExpr:
+					name = t.X.(*ast.Ident).Name
+				}
+
+				b, ok := bindings[name]
+				if !ok {
+					b := new(Binding)
+					b.ClassName = name
+					bindings[name] = b
+				}
 
 				// class
 				if r.Names == nil {
-
+					b.ClassMethods = append(b.ClassMethods, n)
 				} else {
-					// instance
-				}
-				if n.Recv.List[0].Type {
-					binding.ClassMethods = append(binding.ClassMethods, n)
+					b.InstanceMethods = append(b.InstanceMethods, n)
 				}
 			}
 		case *ast.TypeSpec:
-			binding.ClassName = n.Name.Name
+			bindings[n.Name.Name] = &Binding{
+				ClassName: n.Name.Name,
+			}
 
 		}
 
 		return true
 	})
-	log.Printf("%+v", binding)
 	o := jen.NewFile(*out)
-	
-	o.Func().Id("main").Params().Block(jen.Qual("log", "Println").Call(jen.Lit("Hello world")))
-	b := new(bytes.Buffer)
-	err = o.Render(b)
+
+	for _, x := range bindings {
+		x.BindMethods(o)
+	}
+
+	var b bytes.Buffer
+	err = o.Render(&b)
 	log.Println(err, b.String())
 }
