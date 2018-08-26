502a5f7c2747db07280d1742b527cc16a2cd1c16
hachi8833 <hachi8833@gmail.com>
2018-08-26 13:15:56 +0900

    Add test for REPL mode to compiler.go

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -69,3 +69,79 @@ func TestCompileToInstructionsNormalModePanic(t *testing.T) {
 		ci[0].Instructions[0].AnchorLine()
 	}, "The code did not panic")
 }
+
+func TestCompileToInstructionsREPLMode(t *testing.T) {
+
+	ci, err := CompileToInstructions(`
+def bar(a)
+  99 + a
+end
+while true do
+end
+`, parser.REPLMode)
+
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if e, a := uint8(10), ci[0].Instructions[0].Opcode; e != a {
+		t.Fatalf("Expect `%d` for first instruction opcode. got: %d", e, a)
+	}
+
+	if e, a := "putobject: 99. source line: 3", ci[0].Instructions[0].Inspect(); e != a {
+		t.Fatalf("Expect `%s` for first instruction inspect. got: %s", e, a)
+	}
+
+	// TODO: change the following simple public functions to public variables
+	if e, a := "bar", ci[0].Name(); e != a {
+		t.Fatalf("Expect `%s` for instruction set name. got: %s", e, a)
+	}
+
+	if e, a := "Def", ci[0].Type(); e != a {
+		t.Fatalf("Expect `%s` for instruction set type. got: %s", e, a)
+	}
+
+	if e, a := "putobject", ci[0].Instructions[0].ActionName(); e != a {
+		t.Fatalf("Expect `%s` for first instruction action name. got: %s", e, a)
+	}
+
+	if e, a := 0, ci[0].Instructions[0].Line(); e != a {
+		t.Fatalf("Expect `%d` for first instruction line. got: %d", e, a)
+	}
+
+	if e, a := 3, ci[0].Instructions[0].SourceLine(); e != a {
+		t.Fatalf("Expect `%d` for first instruction source line. got: %d", e, a)
+	}
+}
+
+// The tests are related to igb/repl.go
+func TestCompileToInstructionsREPLModeFail(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{`
+iff
+end
+`, "unexpected end Line: 2"},
+		{`
+case
+when 1
+  11
+when 2
+  22
+else
+  99
+end
+`, "expected next token to be WHEN, got INT(1) instead. Line: 2",
+		},
+	}
+
+	for _, tt := range tests {
+		_, err := CompileToInstructions(tt.input, parser.REPLMode)
+
+		if err.Error() != tt.expected {
+			t.Fatalf("Expect `%s` error. got: %s", tt.expected, err.Error())
+		}
+	}
+}
